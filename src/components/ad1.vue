d25774273c547b916372f60687d9f12ed0117220
didi0987 <didi0987@gmail.com>
2017-08-04 16:58:12 -0500

    add ad

---
+++ src/components/ad1.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <div class="article_ad1">
+
+  </div>
+</template>
+<style>
+
+.article_ad1
+{
+background-image: url("/../../resource/images/demo/ad1.jpg");
+width:100%;
+height:200px;
+}
+</style>
